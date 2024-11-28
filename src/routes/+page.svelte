882412ff370aa582590ae05af8eefb778a4567dd
Christian Corona <christian_corona@outlook.com>
2024-11-28 20:51:08 +0100

    feat: Add section-insta component to highlight Instagram journey

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Lenis from '$lib/components/lenis/lenis.svelte';
 	import SectionFirstSteps from '$lib/components/section-first-steps/section-first-steps.svelte';
 	import SectionInspiration from '$lib/components/section-inspiration/section-inspiration.svelte';
+	import SectionInsta from '$lib/components/section-insta/section-insta.svelte';
 	import Intro from '$lib/components/section-intro/section-intro.svelte';
 	import SectionNature from '$lib/components/section-nature/section-nature.svelte';
 	import VerticalDivider from '$lib/components/ui/vertical-divider.svelte';
@@ -16,3 +17,4 @@
 <VerticalDivider />
 <SectionInspiration />
 <VerticalDivider />
+<SectionInsta />
