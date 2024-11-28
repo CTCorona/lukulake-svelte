3488cbe21648b74105b307f744d94c48b398f188
Christian Corona <40803972+CTCorona@users.noreply.github.com>
2024-11-28 20:52:32 +0100

    feat: Add section-insta component to highlight Instagram journey (#9)

---
+++ src/lib/components/section-insta/section-insta.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Heading from '$lib/components/ui/heading.svelte';
+	import { css } from 'styled-system/css';
+</script>
+
+<section class={css({ mb: 32 })} id="insta">
+	<Heading>Empieza el camino en @luku.lake</Heading>
+</section>
