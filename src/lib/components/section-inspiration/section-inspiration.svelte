12b50d6ee1acd55f8869305373afeb55d172e8bd
Christian Corona <40803972+CTCorona@users.noreply.github.com>
2024-11-28 20:41:23 +0100

    feat: Add section-inspiration component to showcase inspirational images (#8)

---
+++ src/lib/components/section-inspiration/section-inspiration.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import inspiration1 from '$lib/assets/images/img-5.jpg?enhanced&quality=80';
+	import inspiration2 from '$lib/assets/images/img-6.jpg?enhanced&quality=80';
+	import inspiration3 from '$lib/assets/images/img-7.jpg?enhanced&quality=80';
+	import Heading from '$lib/components/ui/heading.svelte';
+	import { css } from 'styled-system/css';
+	import { hstack, vstack } from 'styled-system/patterns';
+</script>
+
+<section id="inspiration">
+	<Heading tag="h2">Su curiosidad, nuestra inspiración</Heading>
+	<div class={hstack({ alignItems: 'flex-start', gap: 10 })}>
+		<enhanced:img
+			class={css({ mt: 16 })}
+			src={inspiration3}
+			alt="madre e hija sentadas sobre madera"
+		/>
+		<div class={vstack({ gap: 10 })}>
+			<enhanced:img src={inspiration1} alt="chica abrazando a un conejo" />
+			<enhanced:img
+				class={css({ maxW: '10/12' })}
+				src={inspiration2}
+				alt="madre abrazando a su hija"
+			/>
+		</div>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Lenis from '$lib/components/lenis/lenis.svelte';
 	import SectionFirstSteps from '$lib/components/section-first-steps/section-first-steps.svelte';
+	import SectionInspiration from '$lib/components/section-inspiration/section-inspiration.svelte';
 	import Intro from '$lib/components/section-intro/section-intro.svelte';
 	import SectionNature from '$lib/components/section-nature/section-nature.svelte';
 	import VerticalDivider from '$lib/components/ui/vertical-divider.svelte';
@@ -12,3 +13,6 @@
 <SectionNature />
 <VerticalDivider />
 <SectionFirstSteps />
+<VerticalDivider />
+<SectionInspiration />
+<VerticalDivider />
