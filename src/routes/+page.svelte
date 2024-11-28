9de1681f1ddcd33251cf8413e17e71760561a4ae
Christian Corona <40803972+CTCorona@users.noreply.github.com>
2024-11-28 20:33:27 +0100

    feat: Add section-first-steps component to showcase baby shoe features (#7)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Lenis from '$lib/components/lenis/lenis.svelte';
+	import SectionFirstSteps from '$lib/components/section-first-steps/section-first-steps.svelte';
 	import Intro from '$lib/components/section-intro/section-intro.svelte';
 	import SectionNature from '$lib/components/section-nature/section-nature.svelte';
 	import VerticalDivider from '$lib/components/ui/vertical-divider.svelte';
@@ -9,3 +10,5 @@
 <Intro />
 <VerticalDivider />
 <SectionNature />
+<VerticalDivider />
+<SectionFirstSteps />
